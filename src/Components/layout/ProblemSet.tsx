295326aa9075f7c16001e0863c2682e3d148a573
Abhijith <abhijithasokan831@gmail.com>
2025-08-09 13:50:02 +0530

    receiving problem set from backend

--- src/Components/layout/ProblemSet.tsx
+++ src/Components/layout/ProblemSet.tsx
@@ -1,6 +1,21 @@
-import React from 'react'
+'use client';
+import { getProblems } from '@/app/services/user/userAPI';
+import React, { useEffect } from 'react'
 
 const ProblemSet:React.FC = () => {
+
+  useEffect(()=>{
+      const fetchProblems = async ()=>{
+        try {
+          const result = await getProblems()
+          console.log('ress in problem',result)
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      fetchProblems()
+  },[])
+
   return (
     <div>
       <h1>jsfjsmfsmfd</h1>
--- src/app/services/user/userAPI.ts
+++ src/app/services/user/userAPI.ts
@@ -13,14 +13,22 @@ interface loginData{
 
 export const signUpRequest = async (formData: SignUpData) => {
     
-  const response = await api.post("/signup", formData);
+  const response = await api.post("/auth/signup", formData);
   
   return response.data;
 };
 
 export const loginRequest = async (formData: loginData) => {
     
-  const response = await api.post("/login", formData);
+  const response = await api.post("/auth/login", formData);
   
   return response.data;
 };
+
+
+export const getProblems = async ()=>{
+  
+  const response = await api.get('/problems')
+
+  return response.data;
+}
