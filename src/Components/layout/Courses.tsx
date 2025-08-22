84cad8bb9b86442e2f235dea00fe96274796fc20
Abhijith <abhijithasokan831@gmail.com>
2025-08-22 21:46:07 +0530

    courses component responsiveness

--- src/Components/layout/Courses.tsx
+++ src/Components/layout/Courses.tsx
@@ -35,14 +35,14 @@ const Courses:React.FC = () => {
 
   return (
     <div className="overflow-x-auto scrollbar-hide hide-scrollbar">
-      <div className="flex space-x-6 p-4">
+      <div className="space-x-6 p-4 grid grid-cols-1 sm:grid-cols-2 lg:flex lg:space-x-6 gap-6 lg:gap-0  scrollbar-hide">
         {features.map((feature, index) => (
-          <div  key={index}  className=" relative min-w-[350px]  bg-white shadow-lg rounded-lg  transition hover:scale-101 duration-300">
+          <div  key={index}  className=" relative sm:w-auto lg:min-w-[320px] bg-white shadow-lg rounded-lg  transition hover:scale-101 duration-300">
             <Image  src={feature.image} alt={feature.title} width={400} height={400} className="w-full h-30 object-cover rounded"/>
             <div className='absolute inset-0 left-2 '>
-                <h3 className="text-md font-bold mt-1">{feature.title}</h3>
-                <p className="text-gray-600 text-sm dark:text-gray-300 ">{feature.description}</p>
-                <button className='text-sm rounded-sm bg-white font-bold text-black m-2 mt-4 px-2 py-1 hover:transition-transform hover:scale-101 duration-300'>Start Learning</button>
+                <h3 className="text-xs md:text-md lg:text-lg font-bold mt-1 ">{feature.title}</h3>
+                <p className="text-gray-600 text-xs md:text-md lg:text-md dark:text-gray-300 ">{feature.description}</p>
+                <button className='text-xs md:text-md lg:text-md rounded-xs bg-white font-bold text-black m-2 mt-4 px-2 py-1 hover:transition-transform hover:scale-101 duration-300'>Start Learning</button>
             </div>
           </div>
         ))}
