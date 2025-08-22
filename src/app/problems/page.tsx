da9479edc9163389c93ac17e3030dad7ac4458be
Abhijith <abhijithasokan831@gmail.com>
2025-08-22 21:47:46 +0530

    problems page responsiveness added

--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -4,32 +4,31 @@ import Navbar from '@/Components/layout/Navbar'
 import ProblemSet from '@/Components/layout/ProblemSet'
 import React from 'react'
 
-
-const problems = () => {
-  
-
+const Problems = () => {
   return (
-    <div className='min-h-screen bg-gray-200 dark:bg-gray-950'>
-        <Navbar/>
-        <div className='  mx-8 flex gap-4'>
-            <div className=' w-3/4 rounded-md mt-2 '>
-                <div className='my-auto bg-gray-300 dark:bg-neutral-800 rounded-md '>
-                  <Courses/>
-                </div>              
-                <div className='bg-gray-300 dark:bg-neutral-800  mt-2 rounded-md'>
-                  <Filter/>
-                </div>
-                <div className='bg-gray-300 dark:bg-neutral-800 mt-2 rounded-md'>
-                  <ProblemSet/>
-                </div>
-            </div>
-            <div className='bg-green-400 w-1/4 mt-2 rounded-md' >
-                <h1>sfdknsjk</h1>
-            </div>
+    <div className="min-h-screen bg-gray-200 dark:bg-gray-950">
+      <Navbar />
+      <div className="mx-4 md:mx-8 flex flex-col md:flex-row gap-4">
+        {/* Left Section */}
+        <div className="w-full md:w-3/4 rounded-md mt-2">
+          <div className="my-auto bg-gray-300 dark:bg-neutral-800 rounded-md">
+            <Courses />
+          </div>
+          <div className="bg-gray-300 dark:bg-neutral-800 mt-2 rounded-md">
+            <Filter />
+          </div>
+          <div className="bg-gray-300 dark:bg-neutral-800 mt-2 rounded-md">
+            <ProblemSet />
+          </div>
         </div>
-        
+
+        {/* Right Section */}
+        <div className="bg-green-400 w-full md:w-1/4 mt-2 rounded-md">
+          <h1>sfdknsjk</h1>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default problems
+export default Problems
